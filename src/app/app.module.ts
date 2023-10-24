923e4f2682812e547c88c43607c73a7568b89759
Anthony Toniolli <anthony.toniolli@zazmic.com>
2023-10-24 09:01:04 -0300

    Angular Interview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BackwardStringPipe } from './shared/pipes/backward-string.pipe';
+import { SlotComponent } from './components/slot/slot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackwardStringPipe,
+    SlotComponent,
   ],
   imports: [
     BrowserModule
